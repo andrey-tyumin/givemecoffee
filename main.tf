5ff3698e7146dcffde27768b38e1fa3f3232aa83
Andrey Tyumin <posthus@yandex.ru>
2021-08-25 21:31:13 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,25 +18,11 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-locals {
-	instance_type_map = {
-		stage = "t2.micro"
-		prod = "t2.micro"
-	}
-}
-
-locals {
-	instance_count_map = {
-		stage = 1
-		prod = 2
-	}
-}
-
 
 resource "aws_instance" "hw" {
   ami           = "ami-00399ec92321828f5"
-#  instance_type = local.instance_type_map[terraform.workspace]
-#  count = local.instance_count_map[terraform.workspace]
+  instance_type = t2.micro
+  count = 2
   associate_public_ip_address = "true"
 
   tags = {
@@ -44,17 +30,6 @@ resource "aws_instance" "hw" {
   }
 }
 
-# resource "aws_instance" "hww" {
-#  ami	= "ami-0ba62214afa52bec7"
-#  instance_type = local.instance_type_map[terraform.workspace]
-#  for_each = local.instance_count_map
-#  for_each = { for n in local.instance_count_map : n => "${n}" }
-#  ami = " ami-0ba62214afa52bec7"
-#
-#lifecycle {
-#  create_before_destroy = true
-#  }
-#}
 
 data "aws_region" "current" {}
 
